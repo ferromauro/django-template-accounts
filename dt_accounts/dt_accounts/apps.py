933d883ed2b965ed447bf7fb759262191b2ee0d9
StartG17 <ferromauro@yahoo.it>
2020-04-04 11:13:24 +0200

    First commit of Django Template of account system core named: dt_accounts

---
+++ dt_accounts/dt_accounts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DtAccountsConfig(AppConfig):
+    name = 'dt_accounts'
---
+++ dt_accounts/website/urls.py
@@ -0,0 +1,35 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from dt_accounts import views as accounts_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', accounts_views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view() , name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view() , name='password_reset_complete'),
+    path('mypage/', accounts_views.mypage, name="mypage"),
+
+
+
+]
